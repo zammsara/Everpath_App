e30739a7efd6646aa870b31b642127deec663b22
zammmsara <sara.zambrana@gmail.com>
2026-06-11 18:55:25 -0600

    feat: asociar metas y quests a áreas de vida

--- android-app/app/src/main/java/com/everpath/domain/model/GoalNode.kt
+++ android-app/app/src/main/java/com/everpath/domain/model/GoalNode.kt
@@ -1,11 +1,13 @@
 package com.everpath.domain.model
 
 import com.everpath.domain.enums.GoalStatus
+import com.everpath.domain.enums.LifeAreaType
 
 data class GoalNode(
     val id: String,
     val title: String,
     val description: String,
+    val lifeArea: LifeAreaType,
     val status: GoalStatus,
     val activities: List<Activity>
 )
--- android-app/app/src/main/java/com/everpath/domain/model/Quest.kt
+++ android-app/app/src/main/java/com/everpath/domain/model/Quest.kt
@@ -1,5 +1,6 @@
 package com.everpath.domain.model
 
+import com.everpath.domain.enums.LifeAreaType
 import com.everpath.domain.enums.QuestStatus
 import com.everpath.domain.enums.QuestType
 
@@ -7,6 +8,7 @@ data class Quest(
     val id: String,
     val title: String,
     val description: String,
+    val lifeArea: LifeAreaType,
     val type: QuestType,
     val status: QuestStatus
 )
