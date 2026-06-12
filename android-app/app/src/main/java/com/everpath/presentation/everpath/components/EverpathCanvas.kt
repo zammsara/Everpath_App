9881b9c338a365244006905572531d7554e011ac
zammmsara <sara.zambrana@gmail.com>
2026-06-12 10:52:19 -0600

    feat: crear componente base everpath canvas

---
+++ android-app/app/src/main/java/com/everpath/presentation/everpath/components/EverpathCanvas.kt
@@ -0,0 +1,26 @@
+package com.everpath.presentation.everpath.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun EverpathCanvas(
+    modifier: Modifier = Modifier
+) {
+
+    Canvas(
+        modifier = modifier.fillMaxSize()
+    ) {
+
+        // Próximamente:
+        // GoalConnections
+        // Líneas
+        // Flechas
+        // Relaciones
+
+    }
+
+}
