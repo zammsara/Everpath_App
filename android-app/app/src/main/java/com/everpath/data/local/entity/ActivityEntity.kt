5ea29588e73e862f74a298b422954c19d33982e2
zammmsara <sara.zambrana@gmail.com>
2026-06-13 12:58:21 -0600

    feat: crear entidad room para activities

---
+++ android-app/app/src/main/java/com/everpath/data/local/entity/ActivityEntity.kt
@@ -0,0 +1,34 @@
+package com.everpath.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "activities",
+    foreignKeys = [
+        ForeignKey(
+            entity = GoalEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["goalId"],
+            onDelete = ForeignKey.CASCADE //si se elimina un goal, se eliminan sus actividades asociadas
+        )
+    ],
+    indices = [
+        Index(value = ["goalId"])
+    ]
+)
+data class ActivityEntity(
+
+    @PrimaryKey
+    val id: String,
+
+    val goalId: String,
+
+    val title: String,
+
+    val description: String,
+
+    val status: String
+)
