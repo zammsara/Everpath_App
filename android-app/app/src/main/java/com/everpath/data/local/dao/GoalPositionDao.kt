18aa19a268e438f20a05150e3fd5ff30b5bcf822
zammmsara <sara.zambrana@gmail.com>
2026-06-13 19:02:06 -0600

    feat: implementar dao de posiciones de goals

---
+++ android-app/app/src/main/java/com/everpath/data/local/dao/GoalPositionDao.kt
@@ -0,0 +1,39 @@
+package com.everpath.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.everpath.data.local.entity.GoalPositionEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface GoalPositionDao {
+
+    @Query(
+        "SELECT * FROM goal_positions"
+    )
+    fun getAllPositions():
+            Flow<List<GoalPositionEntity>>
+
+    @Query(
+        "SELECT * FROM goal_positions WHERE goalId = :goalId"
+    )
+    suspend fun getPositionByGoalId(
+        goalId: String
+    ): GoalPositionEntity?
+
+    @Insert(
+        onConflict = OnConflictStrategy.REPLACE
+    )
+    suspend fun savePosition(
+        position: GoalPositionEntity
+    )
+
+    @Query(
+        "DELETE FROM goal_positions WHERE goalId = :goalId"
+    )
+    suspend fun deletePosition(
+        goalId: String
+    )
+}
