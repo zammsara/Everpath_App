605ca786ba827fcef9ff9d80529a8ec6162119d4
zammmsara <sara.zambrana@gmail.com>
2026-06-13 19:03:02 -0600

    feat: registrar posiciones de goals en room

--- android-app/app/src/main/java/com/everpath/data/local/database/EverpathDatabase.kt
+++ android-app/app/src/main/java/com/everpath/data/local/database/EverpathDatabase.kt
@@ -4,15 +4,18 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.everpath.data.local.dao.ActivityDao
 import com.everpath.data.local.dao.GoalDao
+import com.everpath.data.local.dao.GoalPositionDao
 import com.everpath.data.local.entity.ActivityEntity
 import com.everpath.data.local.entity.GoalEntity
+import com.everpath.data.local.entity.GoalPositionEntity
 
 @Database(
     entities = [
         GoalEntity::class,
-        ActivityEntity::class
+        ActivityEntity::class,
+        GoalPositionEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class EverpathDatabase : RoomDatabase() {
@@ -20,4 +23,6 @@ abstract class EverpathDatabase : RoomDatabase() {
     abstract fun goalDao(): GoalDao
 
     abstract fun activityDao(): ActivityDao
+
+    abstract fun goalPositionDao(): GoalPositionDao
 }
