abd2c1b08ba84ba5b40827c7fec1653e5e1c705c
zammmsara <sara.zambrana@gmail.com>
2026-06-21 09:22:46 -0600

    feat: inicializar proyecto Spring Boot de Everpath

---
+++ backend/api/src/test/java/com/everpath/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everpath.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
