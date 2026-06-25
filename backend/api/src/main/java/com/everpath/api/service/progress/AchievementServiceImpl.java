ed2821c035a8715f4ef0063a13fd96b58118c1e3
zammmsara <sara.zambrana@gmail.com>
2026-06-25 15:01:34 -0600

    feat: agregar consultas para estadísticas de achievements

---
+++ backend/api/src/main/java/com/everpath/api/service/progress/AchievementServiceImpl.java
@@ -0,0 +1,11 @@
+package com.everpath.api.service.progress;
+
+/**
+ * Project: api
+ * From: com.everpath.api.service.progress
+ * Created by: Sara Zambrana
+ * On: 25/06/2026 at 14:53
+ * All rights reserved 2026.
+ */
+public class AchievementServiceImpl {
+}
